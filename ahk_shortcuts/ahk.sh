807b4c4ec5bdc6273646fa7e60991c44050e5cfb
Danny Brown <dannybrown37@gmail.com>
2023-08-29 19:30:21 -0400

    feat: add secrets file processing to ahk.sh

--- ahk_shortcuts/ahk.sh
+++ ahk_shortcuts/ahk.sh
@@ -5,11 +5,16 @@
 script=$(readlink -f "$BASH_SOURCE")
 script_path=$(dirname "$script")
 ahk_file_path=$script_path/dev_shortcuts.ahk
+ahk_secrets_path=$script_path/secrets.ahk
 
 # use "open" CLI arg to open the file for edits
 if [ "$1" = "open" ]; then
     code $ahk_file_path
+elif [ "$1" = "open_secrets" ]; then
+    code $ahk_secrets_path
 else
     win_drive_path=$(wslpath -w -a "$ahk_file_path")
+    powershell.exe /c start "${win_drive_path}"
+    win_drive_path=$(wslpath -w -a "$ahk_secrets_path")
     powershell.exe /c start "${win_drive_path}"
 fi
