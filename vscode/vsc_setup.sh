734802f92f5af5b24668f01e09f9c886eb3ee84a
Danny Brown <dannybrown37@gmail.com>
2023-08-29 17:42:11 -0400

    feat: merge default vscode settings into existing settings

--- vscode/vsc_setup.sh
+++ vscode/vsc_setup.sh
@@ -1,17 +1,27 @@
 #!/usr/bin/env bash
 
+script=$(readlink -f "$BASH_SOURCE")
+script_path=$(dirname "$script")
+default_settings_path=$script_path/default_settings.json
+
 # Path to settings.json in WSL
 vscode_settings_path="/mnt/c/Users/${WINDOWS_USERNAME}/AppData/Roaming/Code/User/settings.json"
 
 echo $vscode_settings_path
 
-# Check if file exists
+# Check if settings.json file already exists -- if so, read it
 if [ -f "$vscode_settings_path" ]; then
-
   # Print contents of settings file
-  echo "VSCode settings:"
+  echo "CurrentVSCode settings:"
   cat "$vscode_settings_path"
-
 else
-  echo "Settings file not found at ${vscode_settings_path}"
+  echo "{}" > "$vscode_settings_path"
+  echo "Created an empty settings.json"
 fi
+
+merged_settings=$(jq -s '.[0] * .[1]' "$vscode_settings_path" "$default_settings_path")
+
+echo $merged_settings | jq '.' > "$vscode_settings_path"
+
+echo "New VSCode settings:"
+cat $vscode_settings_path
