f8835973690631e80e426e42c2397ab8dffb07bf
Danny Brown <dannybrown37@gmail.com>
2023-08-30 09:20:59 -0700

    feat: cdp with auto-completion

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Get Windows username (once) to pull/write vscode
+# Get Windows username (once) to interact with Windows side of WSL divide
 if ! grep -q "^export WINDOWS_USERNAME=" "${HOME}/.bashrc"; then
     ls /mnt/c/Users
     read -p "What is your Windows username? " windows_username
@@ -9,6 +9,7 @@ fi
 
 script_path=$(readlink -f "${BASH_SOURCE[0]}")
 script_dir=$(dirname "$script_path")
+
 
 # install apt packages
 apt_packages=(
@@ -23,10 +24,12 @@ apt_packages=(
 )
 sudo apt install -y "${apt_packages[@]}"
 
+
 # install pip packages
 pip install --upgrade pip
 pip_packages=()
 pip install "${pip_packages[@]}"
+
 
 # autoenv automatically runs .env file when you cd in
 curl -#fLo- 'https://raw.githubusercontent.com/hyperupcall/autoenv/master/scripts/install.sh' | sh
@@ -44,11 +47,29 @@ for command in "${bashrc_commands[@]}"; do
     fi
 done
 
+
 # add default bash config to ~/.bashrc
 source bashrc/setup_bashrc.sh
 
-source ~/.bashrc
-echo Internal commands and environment variables installed
 
 # setup VSCode with default settings and extensions
 source $script_dir/vscode/vsc_setup.sh
+
+
+# source all files in path_scripts
+path_scripts_dir="$script_dir/path_scripts"
+if [ -d "$path_scripts_dir" ]; then
+    # Loop through each script file and source it
+    for script_file in "$path_scripts_dir"/*.sh; do
+        if [ -f "$script_file" ]; then
+            source "$script_file"
+            echo "Sourced: $script_file"
+        fi
+    done
+else
+    echo "Directory '$path_scripts_dir' does not exist."
+fi
+
+
+source ~/.bashrc
+echo Setup complete!
