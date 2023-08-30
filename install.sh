da2c5f6305a963bf664a430f3a5bb489674a712b
Danny Brown <danny.brown@canoo.com>
2023-08-30 16:13:09 -0700

    chore: comment autoenv for now; maybe investigate better options

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ source $root_dir/bash/profile.sh  # Bash profile
 source $root_dir/vscode/vsc_setup.sh  # VSCode settings.json and extensions
 
 # autoenv automatically runs .env file when you cd in
-curl -#fLo- 'https://raw.githubusercontent.com/hyperupcall/autoenv/master/scripts/install.sh' | sh
+# curl -#fLo- 'https://raw.githubusercontent.com/hyperupcall/autoenv/master/scripts/install.sh' | sh
 
 source ~/.bashrc
 echo dotfiles setup complete!
