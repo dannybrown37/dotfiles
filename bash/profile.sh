30a4657cd1377241b0d1cd6b3e339aa8d2b9aef2
Danny Brown <dannybrown37@gmail.com>
2023-08-30 13:39:07 -0700

    feat: revamp bash profile setup, use a series of dotfiles and add one call to ~/.bashrc

---
+++ bash/profile.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+script=$(readlink -f "$BASH_SOURCE")
+script_path=$(dirname "$script")
+
+
+# Run all dotfiles
+installation_order=(
+    .apt
+    .functions
+    .envvars
+    .aliases
+    .scripts
+)
+
+for dotfile in "${installation_order[@]}"; do
+    source $script_path/$dotfile
+done
+
+
+# Add this script to .bashrc (if it's not already there)
+lines_for_bash_rc=(
+    "# Set up bash profile from dotfiles repo"
+    "source $script"
+)
+for line in "${lines_for_bash_rc[@]}"; do
+    if ! grep -qF "$line" "$user_bashrc_file"; then
+        echo $line >> "${HOME}/.bashrc"
+    fi
+done
--- install.sh
+++ install.sh
@@ -7,69 +7,13 @@ if ! grep -q "^export WINDOWS_USERNAME=" "${HOME}/.bashrc"; then
     echo "export WINDOWS_USERNAME=$windows_username" >> ~/.bashrc
 fi
 
-script_path=$(readlink -f "${BASH_SOURCE[0]}")
-script_dir=$(dirname "$script_path")
+root_dir=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
 
-
-# install apt packages
-apt_packages=(
-    bash-completion
-    fzf
-    jq
-    man-db
-    nodejs
-    npm
-    python3
-    python3-distutils
-)
-sudo apt install -y "${apt_packages[@]}"
-
-
-# install pip packages
-pip install --upgrade pip
-pip_packages=()
-pip install "${pip_packages[@]}"
-
+source $root_dir/bash/profile.sh  # Bash profile
+source $root_dir/vscode/vsc_setup.sh  # VSCode settings.json and extensions
 
 # autoenv automatically runs .env file when you cd in
 curl -#fLo- 'https://raw.githubusercontent.com/hyperupcall/autoenv/master/scripts/install.sh' | sh
 
-
-# "install" internal utilities by using alias >> .bashrc
-bashrc_commands=(
-    "alias cht=$script_dir/cht/cht.sh"
-    "alias ahk=$script_dir/ahk_shortcuts/ahk.sh"
-)
-
-for command in "${bashrc_commands[@]}"; do
-    if ! grep -Fxq "$command" ~/.bashrc; then
-        echo "$command" >> ~/.bashrc
-    fi
-done
-
-
-# add default bash config to ~/.bashrc
-source bashrc/setup_bashrc.sh
-
-
-# setup VSCode with default settings and extensions
-source $script_dir/vscode/vsc_setup.sh
-
-
-# source all files in path_scripts
-path_scripts_dir="$script_dir/path_scripts"
-if [ -d "$path_scripts_dir" ]; then
-    # Loop through each script file and source it
-    for script_file in "$path_scripts_dir"/*.sh; do
-        if [ -f "$script_file" ]; then
-            source "$script_file"
-            echo "Sourced: $script_file"
-        fi
-    done
-else
-    echo "Directory '$path_scripts_dir' does not exist."
-fi
-
-
 source ~/.bashrc
-echo Setup complete!
+echo dotfiles setup complete!
