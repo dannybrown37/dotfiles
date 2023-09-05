061421f88060d85e493e9221ed89fbcfd0487b42
Danny Brown <danny.brown@canoo.com>
2023-09-04 17:23:27 -0700

    feat: add bitbucket detection and functionality to pr.sh

--- path_scripts/pr.sh
+++ path_scripts/pr.sh
@@ -6,6 +6,24 @@
 
 pr() {
 
+    if git remote -v | grep -q "bitbucket"; then
+        repo_home="bitbucket"
+        if [[ -z $BITBUCKET_TOKEN ]]; then
+            echo "Error: You must set your BITBUCKET_TOKEN in the environment"
+            return
+        fi
+    elif git remote -v | grep -q "github"; then
+        repo_home="github"
+        if [[ -z $GITHUB_USERNAME || -z $GITHUB_TOKEN ]]; then
+            echo "Error: You must set your GITHUB_USERNAME and GITHUB_TOKEN in the environment"
+            return
+        fi
+    else
+        echo "Error: Repo's remote URL is not supported. Add it or stick with GitHub or Bitbucket."
+        git remote -v
+        return
+    fi
+
     # Get default branch
     default_branch=$(git remote show origin | grep 'HEAD branch' | awk '{print $NF}')
 
@@ -21,24 +39,52 @@ pr() {
     for message in "${commit_messages[@]}"; do
         commit_message+="\n$message"
     done
+    repo_name=$(basename "$(git rev-parse --show-toplevel)")
 
     # Create PR content
-    json_content="{
-        \"title\": \"$pull_request_title\",
-        \"body\": \"$commit_message\",
-        \"head\": \"$current_branch\",
-        \"base\": \"$default_branch\"
-    }"
-    echo "$json_content" > temp_pr.json
 
-    # Create URL to use for PR
-    repo_name=$(basename "$(git rev-parse --show-toplevel)")
-    url="https://api.github.com/repos/$GITHUB_USERNAME/$repo_name/pulls"
+    if [ $repo_home = "bitbucket" ]; then
+        json_content="{
+            \"title\": \"$pull_request_title\",
+            \"description\": \"$commit_message\",
+            \"source\": {
+                \"branch\": {
+                    \"name\": \"$current_branch\"
+                }
+            },
+            \"destination\": {
+                \"branch\": {
+                    \"name\": \"$default_branch\"
+                }
+            }
+        }"
+        echo "$json_content" > temp_pr.json
 
-    curl -X POST -H "Authorization: Bearer $GITHUB_TOKEN" \
-         -H "Accept: application/vnd.github.v3+json" \
-         -d @temp_pr.json \
-         "$url"
+        url="$BITBUCKET_BASE_URL/$repo_name/pull-requests"
+
+        curl -X POST \
+             -H "Authorization: Bearer $BITBUCKET_TOKEN" \
+             -H "Content-Type: application/json" \
+             -d @temp_pr.json \
+             "$url"
+
+    elif [ $repo_home = "github" ]; then
+        json_content="{
+            \"title\": \"$pull_request_title\",
+            \"body\": \"$commit_message\",
+            \"head\": \"$current_branch\",
+            \"base\": \"$default_branch\"
+        }"
+        echo "$json_content" > temp_pr.json
+
+        url="https://api.github.com/repos/$GITHUB_USERNAME/$repo_name/pulls"
+
+        curl -X POST -H "Authorization: Bearer $GITHUB_TOKEN" \
+            -H "Accept: application/vnd.github.v3+json" \
+            -d @temp_pr.json \
+            "$url"
+
+    fi
 
     rm -f temp_pr.json
 }
