86936f9aee489e5f7952650b1e6e83e2cd2e3aa1
Danny Brown <danny.brown@canoo.com>
2023-09-06 11:29:47 -0700

    fix: clean up bash/profile.sh and how it is called/used (switch from --no_installs to --install)

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 root_dir=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
 
-source $root_dir/bash/profile.sh  # Bash profile
+source $root_dir/bash/profile.sh --install  # Bash profile with installations
 source $root_dir/vscode/vsc_setup.sh  # VSCode settings.json and extensions
 
 # autoenv automatically runs .env file when you cd in
-# curl -#fLo- 'https://raw.githubusercontent.com/hyperupcall/autoenv/master/scripts/install.sh' | sh
+curl -#fLo- 'https://raw.githubusercontent.com/hyperupcall/autoenv/master/scripts/install.sh' | sh
 
 source ~/.bashrc
 echo dotfiles setup complete!
