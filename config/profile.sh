26078ca7692092268e22ca96b5c9d5864713e493
Danny Brown <dannybrown37@gmail.com>
2024-06-26 20:56:23 -0400

    whoops, deleted the config files

---
+++ config/profile.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+script=$(readlink -f "$BASH_SOURCE")
+profile_path=$(dirname "$script")
+
+# Source these files every time this file is run regardless of flag
+profile_files=(
+        .envvars
+        .functions
+        .aliases
+        .secrets
+        .aws
+    )
+
+for dotfile in "${profile_files[@]}"; do
+    touch $profile_path/$dotfile
+    source $profile_path/$dotfile
+done
+
+# all scripts in scripts/ directory must be sourced to pick up changes
+source $profile_path/../scripts/source_all.sh
+
+# One time, add this script to ~/.bashrc (without --install flag)
+lines_for_bash_rc=(
+    "# Set up bash profile from dotfiles repo"
+    "source $script"
+)
+for line in "${lines_for_bash_rc[@]}"; do
+    if ! grep -qF "$line" "${HOME}/.bashrc"; then
+        echo $line >> "${HOME}/.bashrc"
+    fi
+done
+
+if [ -z "$WSL_DISTRO_NAME" ]; then
+    cd  # on GitBash, start in ~
+fi
