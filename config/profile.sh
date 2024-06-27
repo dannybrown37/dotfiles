643437b929ba10e9c360717d5800d10697face35
Danny Brown <dannybrown37@gmail.com>
2024-06-26 21:44:03 -0400

    refactor how installs work

--- config/profile.sh
+++ config/profile.sh
@@ -3,27 +3,28 @@
 script=$(readlink -f "$BASH_SOURCE")
 profile_path=$(dirname "$script")
 
-# Source these files every time this file is run regardless of flag
+# Source these files every time this file is run
 profile_files=(
-        .envvars
-        .functions
-        .aliases
-        .secrets
-        .aws
-    )
+    .envvars
+    .functions
+    .aliases
+    .secrets
+    .aws
+)
 
 for dotfile in "${profile_files[@]}"; do
     touch $profile_path/$dotfile
     source $profile_path/$dotfile
 done
 
-# all scripts in scripts/ directory must be sourced to pick up changes
+# All scripts in scripts/ directory must be sourced to pick up changes to them
+# All scripts in that directory are called by their name without the `sh`
 source $profile_path/../scripts/source_all.sh
 
 # One time, add this script to ~/.bashrc (without --install flag)
 lines_for_bash_rc=(
     "# Set up bash profile from dotfiles repo"
-    "source $script"
+    "source ${script}"
 )
 for line in "${lines_for_bash_rc[@]}"; do
     if ! grep -qF "$line" "${HOME}/.bashrc"; then
--- install.sh
+++ install.sh
@@ -1,11 +1,60 @@
 #!/usr/bin/env bash
 
+usage() {
+    echo "Usage: $0 [--install <arg>]"
+    echo "  --install <arg>   Install the application with argument <arg>"
+    echo "Valid args:"
+    echo "  python"
+    echo "  node"
+    echo "  golang"
+    echo "  rust"
+    echo "  vscode"
+    echo "  all"
+    exit 1
+}
+
 root_dir=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
 
-source $root_dir/bash/profile.sh --install  # includes apt, npm, and go installs
+if [[ "$1" = "--help" ]]; then
+    usage
+fi
 
-source $root_dir/vscode/vsc_setup.sh  # VSCode settings.json and extensions
+if [ "$1" = "--install" ]; then
+    if [ $# -gt 1 ]; then
+        INSTALL_ARG="$2"
+    else
+        echo "Error: --install requires an argument"
+        usage
+    fi
+else
+    echo "Error: Unknown option: $1"
+    usage
+fi
+
+source $root_dir/install/.bash
+
+if [[ "$INSTALL_ARG" = "all" || "$INSTALL_ARG" = "python" ]]; then
+    source $root_dir/install/.python
+fi
+
+if [[ "$INSTALL_ARG" = "all" || "$INSTALL_ARG" = "node" ]]; then
+    source $root_dir/install/.node
+fi
+
+if [[ "$INSTALL_ARG" = "all" || "$INSTALL_ARG" = "golang" ]]; then
+    source $root_dir/install/.golang
+fi
+
+if [[ "$INSTALL_ARG" = "all" || "$INSTALL_ARG" = "rust" ]]; then
+    source $root_dir/install/.rust
+fi
+
+if [[ "$INSTALL_ARG" = "all" || "$INSTALL_ARG" = "vscode" ]]; then
+    source $root_dir/vscode/vsc_setup.sh
+fi
+
+source $root_dir/bash/profile.sh
 
 source ~/.bashrc
 
-echo dotfiles setup complete!
+echo "dotfiles setup complete!"
