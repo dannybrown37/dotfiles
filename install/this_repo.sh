5f84fdeb762beeda4396d97c9eeabfb91bec2b2c
Danny Brown <dannybrown37@gmail.com>
2024-06-28 10:33:07 -0400

    use npm to install autoenv instead of curl; add instructions to this_repo.sh

--- install/this_repo.sh
+++ install/this_repo.sh
@@ -6,3 +6,15 @@ cd projects || exit
 git clone https://www.github.com/dannybrown37/dotfiles
 cd dotfiles || exit
 ./setup
+echo "Bash profile configured!"
+echo "You can install the following development environment setups:"
+echo "  ./setup --install <language>"
+echo "      python"
+echo "      node"
+echo "      golang"
+echo "      rust"
+echo "      vscode"
+echo "Or full the full suite:"
+echo "      all"
+echo
+echo "Sync bash profile/apt packages at any time with '. ~/setup'"
