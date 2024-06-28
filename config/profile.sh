74dbb140d5f01b9a34a2bad8b384cf48def64e3d
Danny Brown <dannybrown37@gmail.com>
2024-06-28 12:53:41 -0400

    shellcheck and Git Bash symbol

--- config/profile.sh
+++ config/profile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-script=$(readlink -f "$BASH_SOURCE")
+script=$(readlink -f "${BASH_SOURCE[0]}")
 profile_path=$(dirname "$script")
 
 # Source these files every time this file is run
@@ -13,13 +13,13 @@ profile_files=(
 )
 
 for dotfile in "${profile_files[@]}"; do
-    touch $profile_path/$dotfile
-    source $profile_path/$dotfile
+    touch "$profile_path/$dotfile"
+    source "$profile_path/$dotfile"
 done
 
 # All scripts in scripts/ directory must be sourced to pick up changes to them
 # All scripts in that directory are called by their name without the `sh`
-source $profile_path/../scripts/source_all.sh
+source "$profile_path/../scripts/source_all.sh"
 
 # One time, add this script to ~/.bashrc (without --install flag)
 lines_for_bash_rc=(
@@ -28,10 +28,10 @@ lines_for_bash_rc=(
 )
 for line in "${lines_for_bash_rc[@]}"; do
     if ! grep -qF "$line" "${HOME}/.bashrc"; then
-        echo $line >> "${HOME}/.bashrc"
+        echo "$line" >> "${HOME}/.bashrc"
     fi
 done
 
 if [ -z "$WSL_DISTRO_NAME" ]; then
-    cd  # on GitBash, start in ~
+    cd || exit # on GitBash, start in ~
 fi
