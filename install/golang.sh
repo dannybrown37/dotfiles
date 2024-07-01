d7f865954290fa34e7abc494a7640470eff49b55
Danny Brown <dannybrown37@gmail.com>
2024-06-30 21:09:29 -0400

    improve golang install

--- install/golang.sh
+++ install/golang.sh
@@ -17,32 +17,4 @@ sudo tar -xvf "${file_name}"
 sudo mv go /usr/local
 rm "${file_name}"
 
-
-##
-## Copy golang configuration to .bashrc file if it's not already there
-##
-
-# shellcheck disable=SC2016
-GOLANG_SETUP='
-export GOROOT=/usr/local/go
-export GOPATH=$HOME/go
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-'
-
-needs_update=false
-while IFS= read -r line; do
-    if ! grep -Fxq "${line}" ~/.bashrc; then
-        needs_update=true
-        break
-    fi
-done <<< "${GOLANG_SETUP}"
-
-if [[ "${needs_update}" = true ]]; then
-    echo "${GOLANG_SETUP}" >> ~/.bashrc
-    echo "golang config variables added to .bashrc"
-else
-    echo "golang config variables already exist in .bashrc"
-fi
-
-# shellcheck disable=SC1090
-. ~/.bashrc
+source "${HOME}/.bashrc"
