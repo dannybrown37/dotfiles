570ce2adfca8d9cba66b252b2b04b6137a5f0813
Danny Brown <dannybrown37@gmail.com>
2024-07-03 11:32:58 -0400

    have a repo-level settings.json and a synced-settings.json for symlink approximate with Windows

--- .vscode/sync_vsc_settings.sh
+++ .vscode/sync_vsc_settings.sh
@@ -3,34 +3,34 @@
 source ~/.bashrc
 
 vscode_setup_script_path=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
-default_settings_path="${vscode_setup_script_path}/settings.json"
+synced_settings_path="${vscode_setup_script_path}/synced-settings.json"
 
 # Path to settings.json in WSL
 if [[ -z "${WINDOWS_USERNAME}" ]]; then
   echo "Error: no Windows username found in environment"
   exit 1
 fi
-vscode_settings_path="/mnt/c/Users/${WINDOWS_USERNAME}/AppData/Roaming/Code/User/settings.json"
+windows_settings_path="/mnt/c/Users/${WINDOWS_USERNAME}/AppData/Roaming/Code/User/settings.json"
 
-echo "Installing from ${vscode_settings_path}"
+echo "Installing from ${windows_settings_path}"
 
 # Check if settings.json file already exists -- if so, read it
-if [[ -f "${vscode_settings_path}" ]]; then
+if [[ -f "${windows_settings_path}" ]]; then
   # Print contents of settings file
   echo "CurrentVSCode settings:"
-  cat "${vscode_settings_path}"
+  cat "${windows_settings_path}"
 else
-  echo "{}" > "${vscode_settings_path}"
+  echo "{}" > "${windows_settings_path}"
   echo "There is not an existing VSCode settings path in Windows"
 fi
 
 # merge existing and default settings
-merged_settings=$(jq -s '.[0] * .[1]' "${vscode_settings_path}" "${default_settings_path}")
+merged_settings=$(jq -s '.[0] * .[1]' "${windows_settings_path}" "${synced_settings_path}")
 
-echo "${merged_settings}" | jq '.' > "${vscode_settings_path}"
-echo "${merged_settings}" | jq '.' > "${default_settings_path}"
+echo "${merged_settings}" | jq '.' > "${windows_settings_path}"
+echo "${merged_settings}" | jq '.' > "${synced_settings_path}"
 
 echo "New VSCode settings:"
-cat "${vscode_settings_path}"
+cat "${windows_settings_path}"
 
 echo "VSCode settings synced between Windows and WSL"
