c502804ffe4f75b05df34889dce076b5746008b5
Danny Brown <dannybrown37@gmail.com>
2024-07-04 12:14:50 -0400

    goodbye, ./setup -- hello Makefile

--- install/this_repo.sh
+++ install/this_repo.sh
@@ -1,24 +1,11 @@
 #!/usr/bin/bash
 
 sudo apt update
-sudo apt install git -y
+sudo apt install git make -y
 cd ~ || exit
 mkdir projects 2>/dev/null
 cd projects || exit
 git clone https://www.github.com/dannybrown37/dotfiles
 cd dotfiles || exit
-./setup
-echo "Bash profile configured!"
-echo "You can install the following development environment setups:"
-echo "  ./setup --install <language>"
-echo "      python"
-echo "      node"
-echo "      golang"
-echo "      rust"
-echo "      vscode"
-echo "Or full the full suite:"
-echo "      all"
-echo
-echo "Sync bash profile/apt packages at any time with '. ~/setup'"
-
-. ~/.bashrc
+make bash
+make help
