4cf2599bf4f5fd66b56882e519372185c10402ec
Danny Brown <dannybrown37@gmail.com>
2024-07-09 09:05:06 -0400

    move icon config into its own file as well, init.lua is so clean now

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,32 +14,10 @@ require("bashconfig")
 require("experimental")
 require("lazysetup")
 
--- NOTE:[[ Configure and install plugins ]]
---  To check the current status of your plugins, run `:Lazy`
---  You can press `?` in this menu for help. Use `:q` to close the window
---  To update plugins you can run `:Lazy update`
 require("lazy").setup({
-	{ import = "plugins" },
+	{ import = "plugins" }, -- one plugin config per file in lua/plugins
 }, {
-	ui = {
-		-- If you are using a Nerd Font: set icons to an empty table which will use the
-		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-		icons = vim.g.have_nerd_font and {} or {
-			cmd = "⌘",
-			config = "🛠",
-			event = "📅",
-			ft = "📂",
-			init = "⚙",
-			keys = "🗝",
-			plugin = "🔌",
-			runtime = "💻",
-			require = "🌙",
-			source = "📄",
-			start = "🚀",
-			task = "📌",
-			lazy = "💤 ",
-		},
-	},
+	ui = require("iconconfig"),
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
