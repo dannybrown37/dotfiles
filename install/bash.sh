61f0de49e05623759af611e9146dca8a35ff7130
Danny Brown <dannybrown37@gmail.com>
2024-08-08 08:48:28 -0400

    set up cron config

--- install/bash.sh
+++ install/bash.sh
@@ -8,6 +8,7 @@
 apt_packages=(
     bash-completion
     bat
+    cron
     curl
     fzf
     git
@@ -43,6 +44,17 @@ for package in "${apt_packages[@]}"; do
         sudo apt install -y "${package}"
     fi
 done
+
+##
+## Set up cron and cron jobs from repo
+##
+
+sudo systemctl enable cron
+crontab "$HOME/projects/dotfiles/cron/jobs.crontab"
+echo "Cron jobs have been set up from dotfiles."
+crontab -l
+mkdir "$HOME/cron_logs"
+
 
 ##
 ## Install zoxide, per creator, Debian/Ubuntu have old versions in apt
