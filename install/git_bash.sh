f7bc32907d2f4501c5c36e495639aef51361f75d
Danny Brown <dannybrown37@gmail.com>
2024-08-16 15:26:11 -0400

    check before installing jq

--- install/git_bash.sh
+++ install/git_bash.sh
@@ -8,10 +8,13 @@ powershell.exe -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scop
 
 # Install Scoop, a Windows package manager
 powershell.exe -Command "Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression"
-source "$HOME/scoop/apps/scoop/current/scoop-completion.bash"
 
 # Use powershell to install jq as admin, there will be a pop-up prompt
-powershell.exe -Command "Start-Process curl -ArgumentList '-L -o /usr/bin/jq.exe https://github.com/jqlang/jq/releases/latest/download/jq-win64.exe' -Verb RunAs"
+if ! command -v jq &> /dev/null; then
+    powershell.exe -Command "Start-Process curl -ArgumentList '-L -o /usr/bin/jq.exe https://github.com/jqlang/jq/releases/latest/download/jq-win64.exe' -Verb RunAs"
+else
+    echo "jq is already installed"
+fi
 
 # install winget packages
 winget install \
@@ -37,5 +40,4 @@ pipx install cookiecutter \
 
 
 # TODO: exa?
-# TODO: bat alias?
 
