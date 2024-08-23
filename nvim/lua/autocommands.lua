78c0c0bbc608563fb66dc0b046fce3ab3c7f5d1d
Danny Brown <dannybrown37@gmail.com>
2024-08-23 19:17:14 -0400

    add autocommands for vertical help and hightlighting words under cursor

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -1,7 +1,6 @@
 --#region
 -- NOTE: [[ Basic Autocommands ]] See `:help lua-guide-autocommands`
 
--- Highlight when yanking (copying) text  --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
@@ -10,11 +9,30 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
---  Control white space at end of lines
 vim.api.nvim_create_autocmd("BufWritePre", {
+	desc = "Format file on save by removing trailing whitespace",
 	pattern = "*",
 	callback = function()
 		vim.cmd("%s/\\s\\+$//e")
 		vim.cmd("%s/\\r//e")
 	end,
 })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	desc = "Open help window in vertical split on entering a help buffer",
+	pattern = "*.txt",
+	callback = function()
+		if vim.bo.filetype == "help" then
+			vim.cmd("wincmd L")
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd("CursorHold", {
+	desc = "Highlight word under cursor",
+	callback = function()
+		vim.cmd("match Search /\\V\\<" .. vim.fn.expand("<cword>") .. "\\>/")
+	end,
+})
+
+--#endregion
