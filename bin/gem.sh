8c51a1a6de090f43284b3c4b129dc378859157c1
Danny Brown <dannybrown37@gmail.com>
2024-08-23 16:54:22 -0400

    add env var check in gem.sh

--- bin/gem.sh
+++ bin/gem.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 function ask_gemini_a_question() {
+  if [ -z "$GOOGLE_API_KEY" ]; then
+    echo "Please set the GOOGLE_API_KEY environment variable."
+    return 1
+  fi
   curl -s https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GOOGLE_API_KEY} \
   -H 'Content-Type: application/json' \
   -d "{
