f47602553ea697220cadcaee9638003f02d38e8d
Danny Brown <dannybrown37@gmail.com>
2024-08-23 16:31:25 -0400

    really improve my understanding and messaging of telescope commands and which-key setup

--- nvim/lua/plugins/which_key.lua
+++ nvim/lua/plugins/which_key.lua
@@ -5,12 +5,17 @@ return { -- Useful plugin to show you pending keybinds.
 		require("which-key").setup()
 		-- Document existing key chains
 		require("which-key").register({
-			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-			["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+			-- ["<leader>c"] = { name = "[C]", _ = "which_key_ignore" },
+			-- ["<leader>d"] = { name = "[D]", _ = "which_key_ignore" },
+			-- ["<leader>e"] = { name = "[E]", _ = "which_key_ignore" },
+			-- ["<leader>f"] = { name = "[F]", _ = "which_key_ignore" },
+			["<leader>g"] = { name = "[G]oto (LSP)", _ = "which_key_ignore" },
+			-- ["<leader>h"] = { name = "[H]", _ = "which_key_ignore" },
+			-- ["<leader>i"] = { name = "[I]", _ = "which_key_ignore" },
+			-- ["<leader>r"] = { name = "[R]", _ = "which_key_ignore" },
 			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
 			["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
+			["<leader>y"] = { name = "S[y]mbols (LSP)", _ = "which_key_ignore" },
 		})
 	end,
 }
