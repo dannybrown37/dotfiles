8b10024fa5b1b43ad625c3217aca99fdaaf8b95b
Danny Brown <dannybrown37@gmail.com>
2024-08-24 13:00:28 -0400

    use non-deprecated which-key config but annoyingly lose the prefix functionality that I am mainly using it for

--- nvim/lua/plugins/which_key.lua
+++ nvim/lua/plugins/which_key.lua
@@ -1,21 +1,14 @@
-return { -- Useful plugin to show you pending keybinds.
+return {
 	"folke/which-key.nvim",
-	event = "VimEnter", -- Sets the loading event to 'VimEnter'
-	config = function() -- This is the function that runs, AFTER loading
-		require("which-key").setup()
-		-- Document existing key chains
-		require("which-key").register({
-			-- ["<leader>c"] = { name = "[C]", _ = "which_key_ignore" },
-			-- ["<leader>d"] = { name = "[D]", _ = "which_key_ignore" },
-			-- ["<leader>e"] = { name = "[E]", _ = "which_key_ignore" },
-			-- ["<leader>f"] = { name = "[F]", _ = "which_key_ignore" },
-			["<leader>g"] = { name = "[G]oto (LSP)", _ = "which_key_ignore" },
-			-- ["<leader>h"] = { name = "[H]", _ = "which_key_ignore" },
-			-- ["<leader>i"] = { name = "[I]", _ = "which_key_ignore" },
-			-- ["<leader>r"] = { name = "[R]", _ = "which_key_ignore" },
-			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-			["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-			["<leader>y"] = { name = "S[y]mbols (LSP)", _ = "which_key_ignore" },
-		})
-	end,
+	event = "VeryLazy",
+	opts = {},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
 }
