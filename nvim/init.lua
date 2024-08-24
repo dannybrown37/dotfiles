4481bb327d94fe796bd11e0751207d03b4afaa5d
Danny Brown <dannybrown37@gmail.com>
2024-08-24 12:02:14 -0400

    remove nvim_puppetter, remove lazysetup file and just do init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,7 +6,14 @@
 
 require("settings")
 
-require("lazysetup")
+--  See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
 require("lazy").setup({
 	{ import = "plugins" }, -- one plugin config per file in lua/plugins
 }, {
