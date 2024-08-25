e2e499087699c111411599f45017a53abbf4d823
Danny Brown <dannybrown37@gmail.com>
2024-08-25 16:54:20 -0400

    figured out which-key v3 prefix mapping, yay

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -49,7 +49,7 @@ return -- LSP Configuration & Plugins
 
 				-- Rename the variable under your cursor.
 				--  Most Language Servers support renaming across files, etc.
-				map("<leader>r", vim.lsp.buf.rename, "[R]ename")
+				map("<leader>rs", vim.lsp.buf.rename, "[R]ename [S]ymbol")
 
 				-- Execute a code action, usually your cursor needs to be on top of an error
 				-- or a suggestion from your LSP for this to activate.
