6cd21974189f5fdf5979afc32421f5ad25daf6a6
Danny Brown <dannybrown37@gmail.com>
2024-08-28 19:00:01 -0400

    get rid of nvim-cmp, it was driving me crazy; accept automaven with tab

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -33,7 +33,7 @@ return {
 				disable_keymaps = false,
 				disable_inline_completion = false,
 				keymaps = {
-					accept_suggestion = "<C-y>",
+					accept_suggestion = "<Tab>",
 					clear_suggestion = "<Nop>",
 				},
 			},
@@ -50,57 +50,5 @@ return {
 	config = function()
 		local luasnip = require("luasnip")
 		luasnip.config.setup({})
-
-		local cmp = require("cmp") -- See `:help cmp`
-		cmp.setup({
-
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-
-			completion = { completeopt = "menu,menuone,noinsert,noselect" },
-
-			-- seriously. Please read `:help ins-completion`, it is really good!
-			mapping = cmp.mapping.preset.insert({
-				["<C-n>"] = cmp.mapping.select_next_item(), -- Select the [n]ext item
-				["<C-j>"] = cmp.mapping.select_next_item(),
-				["<C-p>"] = cmp.mapping.select_prev_item(), -- Select the [p]revious item
-				["<C-k>"] = cmp.mapping.select_prev_item(),
-				-- select words from autocomplete list with tab/control
-				["<C-y>"] = cmp.mapping.confirm({ select = true }),
-				["<Esc>"] = cmp.mapping.close(),
-				["<C-Space>"] = cmp.mapping.complete({}), -- Manually trigger a completion dropdown
-
-				-- Think of <c-l> as moving to the right of your snippet expansion.
-				--  So if you have a snippet that's like:
-				--  function $name($args)
-				--    $body
-				--  end
-				--
-				-- <c-l> will move you to the right of each of the expansion locations.
-				-- <c-h> is similar, except moving you backwards.
-				["<C-l>"] = cmp.mapping(function()
-					if luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					end
-				end, { "i", "s" }),
-				["<C-h>"] = cmp.mapping(function()
-					if luasnip.locally_jumpable(-1) then
-						luasnip.jump(-1)
-					end
-				end, { "i", "s" }),
-
-				-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-				--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-			}),
-
-			sources = {
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-				{ name = "path" },
-			},
-		})
 	end,
 }
