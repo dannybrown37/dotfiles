b55aad1e6cf3df9e1088c42501bb4b831baaa607
Danny Brown <dannybrown37@gmail.com>
2024-08-27 20:58:46 -0400

    trying to figure out bash autoformatting and why it can't decide 2/4 space indentation

--- ahk/ahk.sh
+++ ahk/ahk.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-
 # Get autohotkey files from this repo in WSL2, start in Windows environment
 # May need to Powershell Admin run: `Set-ExecutionPolicy RemoteSigned`
-
 
 if [[ -z "${ON_WINDOWS}" ]]; then
     echo "AHK is only available from WSL or Git Bash"
@@ -20,7 +18,7 @@ ahk_secrets_path="${DOTFILES_DIR}/ahk/secrets.ahk"
 all_ahk_files=("${DOTFILES_DIR}/ahk/"*.ahk)
 
 if [[ ! -e "${ahk_secrets_path}" ]]; then
-    echo "#SingleInstance Force" >> "${ahk_secrets_path}"
+    echo "#SingleInstance Force" >>"${ahk_secrets_path}"
 fi
 
 hotstring_definitions=$(grep -oE '::[^:]+::[^:]+' "$hotstrings_path")
@@ -32,25 +30,26 @@ elif [[ "$1" = "open" ]]; then
 elif [[ "$1" = "secrets" ]]; then
     nvim "${ahk_secrets_path}" || code "${ahk_secrets_path}"
 elif [[ "$1" = "kill" ]]; then
-    ahk_pids=$(powershell.exe \
-        "Get-Process AutoHotkey* | Select-Object -ExpandProperty Id"
+    ahk_pids=$(
+        powershell.exe \
+            "Get-Process AutoHotkey* | Select-Object -ExpandProperty Id"
     )
     for pid in ${ahk_pids}; do
         echo "Starting ${pid}"
-        if [[ -n "${WSL_DISTRO_NAME}" ]]; then  # handle WSL
+        if [[ -n "${WSL_DISTRO_NAME}" ]]; then # handle WSL
             powershell.exe "Stop-Process -Id ${pid} '-Force'"
-        else  # handle Git Bash
+        else # handle Git Bash
             powershell.exe "Stop-Process -Id ${pid} -Force"
         fi
     done
 else
     for ahk_file in "${all_ahk_files[@]}"; do
-        if [[ -n "${WSL_DISTRO_NAME}" ]]; then  # handle WSL
+        if [[ -n "${WSL_DISTRO_NAME}" ]]; then # handle WSL
             win_drive_path=$(wslpath -w -a "${ahk_file}")
-        else  # handle Git Bash
+        else # handle Git Bash
             win_drive_path=$(cygpath -w -a "${ahk_file}")
         fi
         powershell.exe \
-            -Command "Start-Process '${win_drive_path}'" 2> /dev/null
+            -Command "Start-Process '${win_drive_path}'" 2>/dev/null
     done
 fi
