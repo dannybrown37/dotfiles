01d0bbca86b04ee847cd5f0723b11839b5c37451
Danny Brown <dannybrown37@gmail.com>
2024-09-30 16:28:10 -0400

    add yazi install and y function -- may need more config here

--- install/rust.sh
+++ install/rust.sh
@@ -4,3 +4,12 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 . ~/.bashrc
+
+rustup update
+
+
+##
+## Install cargo packages
+##
+
+cargo install --locked yazi-fm yazi-cli
