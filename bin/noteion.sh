f275cd0035ed59cea66ec833238133a9fac77be1
Danny Brown <dannybrown37@gmail.com>
2024-10-23 21:51:53 -0400

    further development with a created date for noteion, and a currently non-working noteions function to get the table in the CLI

--- bin/noteion.sh
+++ bin/noteion.sh
@@ -4,35 +4,60 @@
 NOTION_API_URL="https://api.notion.com/v1/pages"
 NOTION_VERSION="2022-06-28"
 
-# Function to add an entry to a Notion database
-noteion() {
-
+notion_validate() {
     if [ -z "$NOTION_NOTES_TOKEN" ]; then
-        echo "Error: noteionS_TOKEN is not set"
-        return
+        echo "Error: NOTION_NOTES_TOKEN is not set"
+        return 1
     fi
-
     if [ -z "$NOTION_NOTES_TABLE_ID" ]; then
         echo "Error: NOTION_NOTES_TABLE_ID is not set"
-        return
+        return 1
     fi
+}
+
+noteions() {
+
+    notion_validate || return 1
+
+    # Send request to Notion API
+    response=$(curl -s -X POST "$NOTION_API_URL/$NOTION_NOTES_TABLE_ID/query" \
+        -H "Authorization: Bearer $NOTION_NOTES_TOKEN" \
+        -H "Content-Type: application/json" \
+        -H "Notion-Version: $NOTION_VERSION")
+
+    echo $response
+
+    # Check for errors
+    if echo "$response" | grep -q '"object": "error"'; then
+        echo "Failed to fetch data from Notion. Response: $response"
+        return 1
+    fi
+
+    # Parse and display results
+    echo "Database contents:"
+    echo "$response" | jq '.results[] | {id: .id, properties: .properties}'
+}
+
+noteion() {
+
+    notion_validate || return 1
 
     local header="$*"
 
     if [ -z "$header" ]; then
-        read -r -p "Enter the header of the note: " header
+        read -r -p "Enter a header for the note: " header
     fi
 
     echo "Enter additional lines for file (empty input to finish):"
-    note_content=""
+    local note_content=""
     while IFS= read -r line; do
         [[ -z "$line" ]] && break
         note_content+="$line"$'\n'
     done
-    note_content=$(echo "$note_content" | sed ':a;N;$!ba;s/\n/\\n/g') # escape newlines for JSON formatting
+    note_content=$(echo -n "$note_content" | sed ':a;N;$!ba;s/\n/\\n/g') # escape newlines for JSON formatting
 
     # Create JSON payload
-    payload=$(
+    local payload=$(
         cat <<EOF
 {
     "parent": {"database_id": "$NOTION_NOTES_TABLE_ID"},
@@ -54,6 +79,11 @@ noteion() {
                     }
                 }
             ]
+        },
+        "Created Date": {
+            "date": {
+                "start": "$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+            }
         }
     }
 }
@@ -61,7 +91,7 @@ EOF
     )
 
     # Send request to Notion API
-    response=$(curl -s -X POST "$NOTION_API_URL" \
+    local response=$(curl -s -X POST "$NOTION_API_URL" \
         -H "Authorization: Bearer $NOTION_NOTES_TOKEN" \
         -H "Content-Type: application/json" \
         -H "Notion-Version: $NOTION_VERSION" \
