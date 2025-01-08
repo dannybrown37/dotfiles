9370d2f3e53a7e0d073f7609e76fa0d30b2f777e
Danny Brown <dannybrown37@gmail.com>
2025-01-08 17:14:49 -0500

    get notion contexts from the multi_select of the table itself, no need for separate table

--- bin/noteion.sh
+++ bin/noteion.sh
@@ -4,7 +4,6 @@
 NOTION_API_URL="https://api.notion.com/v1/pages"
 NOTION_VERSION="2022-06-28"
 PROJECTS_TABLE_ID="1709f04dc8b18065a9a6ffb4b5dbd292"
-CONTEXTS_TABLE_ID="1709f04dc8b180daae50f821ac38d73e"
 
 notion_validate() {
     if [ -z "$NOTION_NOTES_TOKEN" ]; then
@@ -23,7 +22,7 @@ noteion() {
         read -r -p "Enter a header for the note: " header
     fi
 
-    echo "Enter additional lines for file (empty input to finish):"
+    echo "Enter additional details in multiple lines (empty line to finish):"
     local note_content=""
     while IFS= read -r line; do
         [[ -z "$line" ]] && break
@@ -34,7 +33,6 @@ noteion() {
     local contexts=$(get_notion_contexts)
     local context=$(echo "$contexts" | fzf --prompt "Select a context for $header")
 
-    # Create JSON payload
     local payload=$(
         cat <<EOF
 {
@@ -80,7 +78,6 @@ noteion() {
 EOF
     )
 
-    # Send request to Notion API
     local response=$(curl -s -X POST "$NOTION_API_URL" \
         -H "Authorization: Bearer $NOTION_NOTES_TOKEN" \
         -H "Content-Type: application/json" \
@@ -95,86 +92,19 @@ EOF
 }
 
 get_notion_contexts() {
-    local url="https://api.notion.com/v1/databases/${CONTEXTS_TABLE_ID}/query"
+    local url="https://api.notion.com/v1/databases/${PROJECTS_TABLE_ID}/query"
     local response=$(curl -s -X POST "$url" \
-        -H "Authorization: Bearer $NOTION_NOTES_TOKEN" \
-        -H "Content-Type: application/json" \
-        -H "Notion-Version: $NOTION_VERSION")
+          -H "Authorization: Bearer ${NOTION_NOTES_TOKEN}" \
+          -H "Content-Type: application/json" \
+          -H "Notion-Version: 2022-06-28")
 
-    echo "$response" | jq -r '.results[].properties.Name.title[].text.content' | sort
-}
-
-sync_notion_multi_selects_with_contexts_single_page() {
-    local table_id=$1
-    local contexts
-    # Assuming get_notion_contexts is getting the contexts as expected
-    IFS=$'\n' read -rd '' -a contexts <<< "$(get_notion_contexts)"
-
-    # Prepare the context array in JSON format
-    local context_json=""
-    for context in "${contexts[@]}"; do
-        context_json+="{\"name\": \"$context\"},"
-    done
-    # shellcheck disable=SC2001
-    context_json=$(echo "$context_json" | sed 's/,$//')
-
-    # Now build the JSON payload
-    local payload=$(
-        cat <<EOF
-{
-    "parent": {"database_id": "$table_id"},
-    "properties": {
-        "Header": {
-            "title": [
-                {
-                    "text": {
-                        "content": "Sync record"
-                    }
-                }
-            ]
-        },
-        "Details": {
-            "rich_text": [
-                {
-                    "text": {
-                        "content": ""
-                    }
-                }
-            ]
-        },
-        "Created Date": {
-            "date": {
-                "start": "$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
-            }
-        },
-        "Context": {
-            "multi_select": [$context_json]
-        },
-        "Status": {
-            "select": {
-                "name": "Triage"
-            }
-        }
-    }
-}
-EOF
-    )
-
-    local response=$(curl -s -X POST "$NOTION_API_URL" \
-        -H "Authorization: Bearer $NOTION_NOTES_TOKEN" \
-        -H "Content-Type: application/json" \
-        -H "Notion-Version: $NOTION_VERSION" \
-        -d "$payload")
-
-    if echo "$response" | grep -q '"id":'; then
-        local page_id=$(echo "$response" | jq -r '.id')  # Extract page id
-        trash_notion_page "$page_id" "$table_id"  # Call the delete function
-    else
-        echo "Failed to add entry. Response: $response"
-    fi
+    local contexts=$(echo "$response" | jq -r '.results[].properties.Context.multi_select[].name' | sort | uniq | grep -v "Pending Context")
+    local final_contexts="Pending Context"$'\n'"$contexts"
+    echo "$final_contexts"
 }
 
 trash_notion_page() {
+    # not currently used but maybe useful in the future
     local page_id=$1
     local table_id=$2
 
@@ -192,9 +122,3 @@ trash_notion_page() {
         echo "Failed to delete page. Response: $delete_response"
     fi
 }
-
-sync_notion_multi_selects_with_contexts_all_pages() {
-    sync_notion_multi_selects_with_contexts_single_page "$PROJECTS_TABLE_ID"
-}
-
-alias sync_notion="sync_notion_multi_selects_with_contexts_all_pages"
