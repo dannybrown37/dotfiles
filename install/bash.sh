ee6559119fd5865f7553b600acf0200db7cd81ea
Danny Brown <dannybrown37@gmail.com>
2025-01-20 12:47:59 -0500

    add git completion

--- install/bash.sh
+++ install/bash.sh
@@ -107,6 +107,13 @@ else
     echo "tmux plugin manager is already installed on this system"
 fi
 
+##
+## curl installs
+##
+
+curl -o ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+
+
 ###
 ### Install atuin shell history manager
 ###
