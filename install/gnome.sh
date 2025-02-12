be76e20b02e0e1b8516aeac4e0be8f60605d3fba
Danny Brown <dannybrown37@gmail.com>
2025-02-11 20:46:54 -0500

    set up some basic gnome settings; fix inputrc bindings

--- install/gnome.sh
+++ install/gnome.sh
@@ -8,6 +8,3 @@ gext enable dash-to-dock@micxgx.gmail.com
 
 gext install just-perfection-desktop@just-perfection
 gext enable just-perfection-desktop@just-perfection
-
-git clone https://github.com/Tudmotu/gnome-shell-extension-clipboard-indicator.git ~/.local/share/gnome-shell/extensions/clipboard-indicator@tudmotu.com
-gext enable clipboard-indicator@tudmotu.com
