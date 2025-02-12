e5c3ae2b167e08bb5fc9d50b7cb81cf9cb1316c3
Danny Brown <dannybrown37@gmail.com>
2025-02-11 19:54:08 -0500

    more precommit fixes

--- .vscode/sync_vsc_settings.sh
+++ .vscode/sync_vsc_settings.sh
@@ -7,8 +7,8 @@ synced_settings_path="${vscode_setup_script_path}/synced-settings.json"
 
 # Path to settings.json in WSL
 if [[ -z "${WINDOWS_USERNAME}" ]]; then
-  echo "Error: no Windows username found in environment"
-  exit 1
+    echo "Error: no Windows username found in environment"
+    exit 1
 fi
 windows_settings_path="/mnt/c/Users/${WINDOWS_USERNAME}/AppData/Roaming/Code/User/settings.json"
 
@@ -16,20 +16,20 @@ echo "Installing from ${windows_settings_path}"
 
 # Check if settings.json file already exists
 if [[ -f "${windows_settings_path}" ]]; then
-  # If the file is empty, initialize it with "{}"
-  [[ ! -s "${windows_settings_path}" ]] && echo "{}" > "${windows_settings_path}"
-  echo "Current VSCode settings:"
+    # If the file is empty, initialize it with "{}"
+    [[ ! -s "${windows_settings_path}" ]] && echo "{}" > "${windows_settings_path}"
+    echo "Current VSCode settings:"
 else
-  # Create the file with "{}" if it does not exist
-  echo "{}" > "${windows_settings_path}"
-  echo "There was no existing VSCode settings file. Created new file with default content."
-  echo "Current VSCode settings:"
+    # Create the file with "{}" if it does not exist
+    echo "{}" > "${windows_settings_path}"
+    echo "There was no existing VSCode settings file. Created new file with default content."
+    echo "Current VSCode settings:"
 fi
 
 # merge existing and default settings
 merged_settings=$(
-  jq -s '.[0] * .[1]' "${windows_settings_path}" "${synced_settings_path}" |
-  jq 'to_entries | sort_by(.key) | from_entries'
+    jq -s '.[0] * .[1]' "${windows_settings_path}" "${synced_settings_path}" |
+    jq 'to_entries | sort_by(.key) | from_entries'
 )
 
 
