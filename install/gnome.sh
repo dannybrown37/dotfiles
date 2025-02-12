814060b93332944bd167a07fc2bd269a7c5c18ae
Danny Brown <dannybrown37@gmail.com>
2025-02-11 19:53:18 -0500

    fix some pre-commit stuff

--- install/gnome.sh
+++ install/gnome.sh
@@ -11,4 +11,3 @@ gext enable just-perfection-desktop@just-perfection
 
 git clone https://github.com/Tudmotu/gnome-shell-extension-clipboard-indicator.git ~/.local/share/gnome-shell/extensions/clipboard-indicator@tudmotu.com
 gext enable clipboard-indicator@tudmotu.com
-
