737c721ee26c5ab75a05545f5480e2cd89020fe8
Danny Brown <dannybrown37@gmail.com>
2026-06-28 14:34:11 -0400

    fix deprecated neovim module

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,20 +1,31 @@
-return { -- Autoformat
+return {
 	"stevearc/conform.nvim",
 	lazy = false,
+	keys = {
+		{
+			"<leader>cf",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			desc = "[C]ode [F]ormat",
+		},
+	},
 	opts = {
 		notify_on_error = false,
-		format_on_save = function(bufnr)
-			local disable_filetypes = { c = true, cpp = true }
-			return {
-				timeout_ms = 500,
-				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-			}
-		end,
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
 		formatters_by_ft = {
+			css = { "prettier" },
+			html = { "prettier" },
+			javascript = { "prettier" },
+			json = { "prettier" },
 			lua = { "stylua" },
-			python = { "isort", "black" }, -- list runs formatters sequentially
-			javascript = { { "prettierd", "prettier" } }, -- sub-list to run *until* formatter found
-			markdown = { "inject" },
+			markdown = { "prettier" },
+			python = { "ruff_format", "ruff_organize_imports" },
+			typescript = { "prettier" },
+			yaml = { "prettier" },
 		},
 	},
 }
