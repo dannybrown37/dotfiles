4969d96b2d2268e4269db49437da2473a39bafcc
Danny Brown <dannybrown37@gmail.com>
2026-07-09 14:36:17 -0400

    use nvim for git

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -21,8 +21,8 @@ return { -- Highlight, edit, and navigate code
 		highlight = { enable = true },
 	},
 	config = function(_, opts) -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-		-- Prefer git instead of curl in order to improve connectivity in some environments
-		require("nvim-treesitter.install").prefer_git = true
+		-- prefer_git = true causes auto_install to fail inside active git sessions (e.g. rebase)
+		require("nvim-treesitter.install").prefer_git = false
 		---@diagnostic disable-next-line: missing-fields
 		require("nvim-treesitter.configs").setup(opts)
 
